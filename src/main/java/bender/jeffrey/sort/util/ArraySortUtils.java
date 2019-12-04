2b8f7ada7ebc795da810caec92a8d84671f6d080
Jeffrey Bender <benderjeffrey1@gmail.com>
2019-12-04 12:07:53 -0500

    Allow Comparator for sort checking to also be for superclass of T. Added Javadocs.

--- src/main/java/bender/jeffrey/sort/util/ArraySortUtils.java
+++ src/main/java/bender/jeffrey/sort/util/ArraySortUtils.java
@@ -8,7 +8,23 @@ import java.util.Comparator;
  */
 public class ArraySortUtils {
 
-    public static <T> boolean isSorted(T[] array, Comparator<T> comparator) {
+    /**
+     * 
+     * Checks if an array is sorted.
+     * Will return {@code true} if:
+     * <ul>
+     *  <li>{@code array} has a {@code length} of 0 - 1.</li>
+     *  <li>If {@code comparator} is not {@code null} and {@code array} is sorted according to the comparison provided in {@code comparator}.</li>
+     *  <li>If {@code comparator} is {@code null} and the class for type of objects in {@code array} implement {@link java.lang.Comparable} and {@code array} is sorted according to the comparison provided from {@link java.lang.Comparable#compareTo(T)} for the class of {@code <T>}.</li>
+     * </ul>
+     * 
+     * @param <T> type of objects in {@code array}
+     * @param array The array to check if it is sorted
+     * @param comparator The comparator to be used to compare objects in the array to determine order.
+     * @return {@code true} if the array is sorted. {@code false} if the array is not sorted.
+     * @throws ClassCastException if the {@code comparator} is null and the class for {@code <T>} does not implement {@link java.lang.Comparable}
+     */
+    public static <T> boolean isSorted(T[] array, Comparator<? super T> comparator) {
         if (array.length > 1) {
             if (comparator == null) {
                 comparator = (o1, o2) -> ((Comparable) o1).compareTo(o2);
@@ -24,6 +40,19 @@ public class ArraySortUtils {
         return true;
     }
 
+    /**
+     * 
+     * Checks if an array is sorted.
+     * Will return true if:
+     * <ul>
+     *  <li>{@code array} has a {@code length} of 0 - 1.</li>
+     *  <li>If {@code array} is sorted according to the comparison provided from {@link java.lang.Comparable#compareTo(T)} for the class of {@code <T>}.</li>
+     * </ul>
+     * 
+     * @param <T> type of objects in {@code array} whose class implements {@link java.lang.Comparable}
+     * @param array The array to check if it is sorted
+     * @return {@code true} if the array is sorted. {@code false} if the array is not sorted.
+     */
     public static <T extends Comparable<? super T>> boolean isSorted(T[] array) {
         return isSorted(array, null);
     }
