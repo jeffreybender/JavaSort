d3545c7186f32218f85d627fb0308301487988d7
Jeffrey Bender <benderjeffrey1@gmail.com>
2019-12-04 11:24:34 -0500

    Cleaned up isSortedMethods

--- src/main/java/bender/jeffrey/sort/util/ArraySortUtils.java
+++ src/main/java/bender/jeffrey/sort/util/ArraySortUtils.java
@@ -10,19 +10,14 @@ public class ArraySortUtils {
 
     public static <T> boolean isSorted(T[] array, Comparator<T> comparator) {
         if (array.length > 1) {
-            if (comparator != null) {
-                for (int i = 1; i < array.length; i++) {
-                    T o1 = array[i - 1];
-                    T o2 = array[i];
-                    if (comparator.compare(o1, o2) > 0) {
-                        return false;
-                    }
-                }
-            } else {
-                if(array[0] != null && array[0] instanceof Comparable) {
-                    return isSorted((Comparable[]) array);
-                } else {
-                    throw new ClassCastException((array[0] != null ? array[0].getClass().getCanonicalName() : "null") + " cannot be cast to type " + Comparable.class.getCanonicalName());
+            if (comparator == null) {
+                comparator = (o1, o2) -> ((Comparable) o1).compareTo(o2);
+            }
+            for (int i = 1; i < array.length; i++) {
+                T o1 = array[i - 1];
+                T o2 = array[i];
+                if (comparator.compare(o1, o2) > 0) {
+                    return false;
                 }
             }
         }
@@ -30,15 +25,6 @@ public class ArraySortUtils {
     }
 
     public static <T extends Comparable<? super T>> boolean isSorted(T[] array) {
-        if (array.length > 1) {
-            for (int i = 1; i < array.length; i++) {
-                T o1 = array[i - 1];
-                T o2 = array[i];
-                if(o1.compareTo(o2) > 0) {
-                    return false;
-                }
-            }
-        }
-        return true;
+        return isSorted(array, null);
     }
 }
