540b33d018670a9a504512e148f1fc64f4b83963
Jeffrey Bender <benderjeffrey1@gmail.com>
2019-12-05 14:36:24 -0500

    Set up tests to use @ParameterizedTest to reuse for each sort class.

--- src/test/java/bender/jeffrey/SortTests.java
+++ src/test/java/bender/jeffrey/SortTests.java
@@ -3,22 +3,42 @@ package bender.jeffrey;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.api.BeforeAll;
 
 import bender.jeffrey.sample.IncomparableClass;
+import bender.jeffrey.sort.BubbleSort;
+import bender.jeffrey.sort.SortUtils;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.lang.reflect.Method;
 import java.util.Comparator;
 
 /**
  *
  * @author jbender
  */
-public abstract class SortTests {
+public class SortTests {
 
     private String[] comparableArray;
     private IncomparableClass[] incomparableArray;
     private final Comparator<String> stringLengthCompare;
     private final Comparator<IncomparableClass> incomperableClassCompare;
+
+    private Method sortWithoutComparatorMethod;
+    private Method sortWithComparatorMethod;
+
+    private static final Class<?>[] sortWithComparatorMethodParameters = new Class[] {
+        Object[].class,
+        Comparator.class
+    };
+
+    private static final Class<?>[] sortWithOutComparatorMethodParameters = new Class[] {
+        Comparable[].class
+    };
+
 
     public SortTests() {
         stringLengthCompare = Comparator.comparing(String::length);
@@ -46,5 +66,32 @@ public abstract class SortTests {
 
     @AfterEach
     public void tearDown() throws Exception {
+    }
+
+    @ParameterizedTest
+    @MethodSource("sortClasses")
+    public void simpleParameterizedTest(Class<?> sortClass) {
+        try {
+            sortWithoutComparatorMethod = getSortWithOutComparatorMethod(sortClass);
+            sortWithoutComparatorMethod.invoke(null, new Object[]{comparableArray});
+            assertTrue(SortUtils.isSorted(comparableArray));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private static Class<?>[] sortClasses() {
+        return new Class<?>[] {
+            BubbleSort.class
+        };
+    }
+
+    private Method getSortWithComparatorMethod(Class<?> sortClass) throws NoSuchMethodException {
+        return sortClass.getMethod("sort", sortWithComparatorMethodParameters);
+    }
+
+    private Method getSortWithOutComparatorMethod(Class<?> sortClass) throws NoSuchMethodException {
+        return sortClass.getMethod("sort", sortWithOutComparatorMethodParameters);
     }
 }
