6e69db4c95ed1e82a962a0d95fd024912a70d3d4
jeffrey <benderjeffrey1@gmail.com>
2022-06-07 21:30:14 -0400

    Added App.java

---
+++ src/main/java/dev/jeffreybender/App.java
@@ -0,0 +1,8 @@
+package dev.jeffreybender;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("HELLO WORLD");
+        System.out.println(System.getProperty("java.version"));
+    }
+}
