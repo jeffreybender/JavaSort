77c892a01fad3adc031ab65ce1543387ad63d4a4
Jeffrey Bender <benderjeffrey1@gmail.com>
2022-06-10 13:02:36 -0400

    undo change

--- src/main/java/dev/jeffreybender/App.java
+++ src/main/java/dev/jeffreybender/App.java
@@ -2,7 +2,7 @@ package dev.jeffreybender;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("HELLO WORLD1");
+        System.out.println("HELLO WORLD");
         System.out.println(System.getProperty("java.version"));
     }
 }
